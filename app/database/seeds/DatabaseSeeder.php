0aed5ca4264b9c864bacca4fdf8d5761d1c48205
Tyler Menezes <tylermenezes@gmail.com>
2015-03-09 10:52:37 -0700

    Fix database seed

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -4,14 +4,16 @@ class DatabaseSeeder extends Seeder {
 
     public function run()
     {
-        $user = new User();
-        $user->username = 'admin';
-        $user->first_name = 'Admin';
-        $user->last_name = 'Admin';
-        $user->email = 'root@localhost';
-        $user->password = Hash::make('password');
-        $user->is_admin = true;
-        $user->save();
+        if (!Models\User::first()) {
+            $user = new User();
+            $user->username = 'admin';
+            $user->first_name = 'Admin';
+            $user->last_name = 'Admin';
+            $user->email = 'root@localhost';
+            $user->password = Hash::make('password');
+            $user->is_admin = true;
+            $user->save();
+        }
     }
 
 }
